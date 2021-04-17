1d8a40ddea8d1355df806a1e830dfb7ee7dbd9ca
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2021-04-17 14:02:44 +0530

    Initial Commit

---
+++ RupeeRepo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RupeeRepo
+//
+//  Created by Pradyumn Shukla on 17/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RupeeRepo/RupeeRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RupeeRepoApp.swift
+//  RupeeRepo
+//
+//  Created by Pradyumn Shukla on 17/04/21.
+//
+
+import SwiftUI
+
+@main
+struct RupeeRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
